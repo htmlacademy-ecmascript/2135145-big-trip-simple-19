ccc70292e6657b8e4cf8661e6576dafaff30a63e
IrinaStyazhkina <irastyazhkina@gmail.com>
2023-01-24 17:22:51 +0300

    style fix

--- src/utils/point.js
+++ src/utils/point.js
@@ -2,15 +2,14 @@ import dayjs from 'dayjs';
 
 const sortByPrice = (pointA, pointB) => {
   if(pointB.basePrice > pointA.basePrice) {
-    return 1
+    return 1;
   } else if(pointA.basePrice > pointB.basePrice){
     return -1;
   }
   return 0;
 };
 
-const sortByDate = (pointA, pointB) => {
-  return dayjs(pointB.dateFrom).diff(pointA.dateFrom);
-};
+const sortByDate = (pointA, pointB) => dayjs(pointB.dateFrom).diff(pointA.dateFrom);
+
 
 export {sortByDate, sortByPrice};
